fa868e3696df4eaee8185e42fe2f4c70c44b023b
noblesaleka <noblesaleka@hotmail.com>
2020-11-07 19:56:30 +0000

    added gallery on homepage

--- cassandra/urls.py
+++ cassandra/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', include('home.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('products/', include('products.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- home/views.py
+++ home/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from products.models import Product
 
 # Create your views here.
 
 def index(request):
     """ A view to return the index page """
+    products = Product.objects.all()
 
-    return render(request, 'home/index.html')
+    context = {
+        'products': products,
+    }
+    return render(request, 'home/index.html', context)
