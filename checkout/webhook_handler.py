8eaa0dd0af235f9ab219f83da1e9d9b980713a0e
noblesaleka <noblesaleka@hotmail.com>
2021-02-01 22:32:11 +0000

    commit before deploy

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -40,8 +40,6 @@ class StripeWH_Handler:
         """
         Handle a payment_intent.succeeded webhook event
         """
-        intent = event.data.object
-        # print(intent)
         return HttpResponse(
             content=f'Unhandled webhook received: {event["type"]}',
             status=200)
@@ -58,7 +56,6 @@ class StripeWH_Handler:
         """
         Handle a payment.succeeded webhook event
         """
-        
         set_paid_until(event.data.object)
         return HttpResponse(
             content=f'Unhandled webhook received: {event["type"]}',
