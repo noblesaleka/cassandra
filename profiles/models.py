9d7cebb469910e08ac97a2194d142b9f1ec8cfc8
noblesaleka <noblesaleka@hotmail.com>
2021-02-04 04:16:49 +0000

    autosave email in profile

--- profiles/models.py
+++ profiles/models.py
@@ -48,6 +48,8 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
     Create or update the user profile
     """
     if created:
-        UserProfile.objects.create(user=instance)
+        print(instance)
+        UserProfile.objects.create(user=instance, default_email=instance.email)
+
     # Existing users: just save the profile
     instance.userprofile.save()
